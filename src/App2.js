f71aa6c7da7b7f9d21989127992a2382ecc3d6fb
anilkumar7838 <anilkumar9868573282@gmail.com>
2022-03-30 18:18:38 +0530

    main

---
+++ src/App2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoList from './component/TodoList/TodoList'
+
+const App2 = () => {
+    return (
+        <>
+            <TodoList/>
+        </>
+    )
+}
+
+export default App2
